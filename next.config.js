207f718ef32ec169f9c9be244a2b792c7f7d9097
Dennis Moldovan <dennismoldovan32@gmail.com>
2023-07-21 20:18:57 +0300

    next config for allowing access to api route

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+    {
+      key: 'X-XSS-Protection',
+      value: '1; mode=block',
+    },
+    {
+      key: 'X-Frame-Options',
+      value: 'SAMEORIGIN',
+    },
+]
+
 const nextConfig = {
     images:{
         remotePatterns:[
@@ -15,7 +27,31 @@ const nextConfig = {
                 pathname:"/*/*"
             },
         ]
-    }
+    },
+    async headers() {
+        return [
+          {
+            source: "/:path*",
+            headers: securityHeaders,
+          },
+          {
+            source: "/api/sendEmail/:api_key",
+            headers: [
+              { key: "Access-Control-Allow-Credentials", value: "true" },
+              { key: "Access-Control-Allow-Origin", value: "*" },
+              {
+                key: "Access-Control-Allow-Methods",
+                value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+              },
+              {
+                key: "Access-Control-Allow-Headers",
+                value:
+                  "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+              },
+            ],
+          },
+        ];
+    },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
