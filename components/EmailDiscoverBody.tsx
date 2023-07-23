082fc518c815d83ee0f77c5c09151cdb0a11195c
Dennis Moldovan <dennismoldovan32@gmail.com>
2023-07-23 20:33:52 +0300

    FIX

--- components/EmailDiscoverBody.tsx
+++ components/EmailDiscoverBody.tsx
@@ -10,8 +10,8 @@ const EmailDiscoverBody = async () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
           
         {templates.map(template=> (
-          <div className="relative">
-           <EmailTemplate key={template._id} id={template._id} clientHtml={template.clientHtml} properties={template.properties}/>
+          <div key={template._id} className="relative">
+            <EmailTemplate id={template._id} clientHtml={template.clientHtml} properties={template.properties}/>
             <CopyBtn value={template._id} className="absolute top-2 right-2 hover:border-slate-700 text-gray-400 dark:text-gray-300 hover:text-gray-400 duration-75 bg-slate-800"/>
           </div>
         ))}
